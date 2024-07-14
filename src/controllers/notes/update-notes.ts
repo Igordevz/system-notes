b0fa0293c73b42bf3b0a407c6c57fc5df1d2783e
Igordevz <programadorigor.dev@gmail.com>
2024-07-13 21:20:20 -0300

    create updateNotes

--- src/controllers/notes/update-notes.ts
+++ src/controllers/notes/update-notes.ts
@@ -1,10 +1,27 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
-import { z } from "zod"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-export default async function UpdateNotes(req:FastifyRequest, reply:FastifyReply){
-  
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+export default async function UpdateNotes(
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { content, title, id }: any = req.body;
 
-  
+  const verifyIdNotes = await prisma.notes.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  const update = await prisma.notes.update({
+    where: {
+      id: id,
+    },
+    data: {
+      content: content || verifyIdNotes?.content,
+      title: title || verifyIdNotes?.title,
+    },
+  });
 }
