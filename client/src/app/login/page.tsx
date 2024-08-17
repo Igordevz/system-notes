1c741aaaf18f0427f81d45274395e6fd5d937a72
Igordevz <programadorigor.dev@gmail.com>
2024-08-17 12:24:14 -0300

    create login and singIn

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React from "react";
 
-export default function page() {
+export default function Login() {
   return (
-    <div>page</div>
-  )
+    <main className="flex flex-col w-full h-screen items-center justify-center">
+      <div className="md:w-[370px] w-5/6">
+        <h1 className="w-full md:text-3xl text-xl font-bold gratuita">
+          Faça seu login
+        </h1>
+      </div>
+      <form className="flex flex-col p-4 gap-2 md:w-[400px] w-5/6">
+        <Label htmlFor="email">E-mail</Label>
+        <Input type="email" name="email" placeholder="seuemail@exemplo.com" />
+        <p className="text-muted-foreground text-sm mb-2">
+          Utilize um e-mail cadastrado para acessar o sistema.
+        </p>
+        <Label htmlFor="password">Senha</Label>
+        <Input type="password" name="password" placeholder="**********" />
+  
+        <Button type="submit">Entrar</Button>
+        <Link href="/register" className="w-full underline text-center">
+          Ainda não tem uma conta? Cadastre-se
+        </Link>
+      </form>
+    </main>
+  );
 }
--- client/src/app/sing-up/page.tsx
+++ client/src/app/sing-up/page.tsx
@@ -1,31 +1,48 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React from "react";
 
-export default function Login() {
+export default function SingUp() {
   return (
-    <main className='flex flex-col w-full h-screen items-center justify-center'>
-      <div className='md:w-[370px] w-5/6'>
-        <h1 className='w-full md:text-3xl text-xl font-bold gratuita'>Criar sua conta </h1>
+    <main className="flex flex-col w-full h-screen items-center justify-center">
+      <div className="md:w-[370px] w-5/6">
+        <h1 className="w-full md:text-3xl text-xl font-bold gratuita">
+          Crie sua conta
+        </h1>
       </div>
-      <form className='flex flex-col p-4 gap-2 md:w-[400px] w-5/6'>
-        <Label htmlFor='name'>Digite seu nome completo</Label>
-        <Input type='name' name='name' placeholder='Nome completo' />
-        <p className='text-muted-foreground text-sm  mb-2'> </p>
-      <hr />
-      <br />
-        <Label htmlFor='email'>Digite seu Email</Label>
-        <Input type='email' name='email' placeholder='example@example.com' />
-        <p className='text-muted-foreground text-sm  mb-2'>Utilize um email válido para realizar login em nosso sistema. </p>
-        <Label htmlFor='password'>Digite sua senha</Label>
-        <Input type='password' name='password'placeholder='**********' />
-        <p className='text-muted-foreground text-sm  mb-2'>Sua senha deve conter no mínimo 8 digitos </p>
-
-        <Button type='submit'>Cadastrar</Button>
-        <Link href='/login' className='w-full underline text-center'>Você já possui uma conta?</Link>
+      <form className="flex flex-col p-4 gap-2 md:w-[400px] w-5/6">
+        <Label htmlFor="name">Nome completo</Label>
+        <Input type="name" name="name" placeholder="Seu nome completo" />
+        <p className="text-muted-foreground text-sm mb-2"> </p>
+        <hr />
+        <br />
+        <Label htmlFor="email">E-mail</Label>
+        <Input type="email" name="email" placeholder="seuemail@exemplo.com" />
+        <p className="text-muted-foreground text-sm mb-2">
+          Utilize um e-mail válido para acessar o sistema.
+        </p>
+        <Label htmlFor="password">Senha</Label>
+        <Input type="password" name="password" placeholder="**********" />
+        <p className="text-muted-foreground text-sm mb-2">
+          A senha deve ter no mínimo 8 caracteres.
+        </p>
+        <div className="flex items-center space-x-2 mb-2">
+          <Checkbox id="terms" />
+          <label
+            htmlFor="terms"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Aceito os termos e condições
+          </label>
+        </div>
+        <Button type="submit">Cadastrar</Button>
+        <Link href="/login" className="w-full underline text-center">
+          Já tem uma conta? Faça login
+        </Link>
       </form>
     </main>
-  )
+  );
 }
