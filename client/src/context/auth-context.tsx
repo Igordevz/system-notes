eefe7423788f86f9f5774ce02faf8fac2cc5cf01
Igordevz <programadorigor.dev@gmail.com>
2024-08-23 21:10:56 -0300

    create use context

--- client/src/context/auth-context.tsx
+++ client/src/context/auth-context.tsx
@@ -3,12 +3,37 @@ import { createContext, useEffect, useState, type ReactNode } from "react";
 import type { IChildren } from "./children-type";
 import { instance } from "@/service/api";
 import { useToast } from "@/components/ui/use-toast"
-
+import Cookies from "js-cookie"
+import { usePathname, useRouter } from "next/navigation";
 export const AuthContextApi = createContext({});
 export default function AuthContextProvider({ children }: IChildren) {
   const { toast } = useToast()
-
+  const pathName = usePathname();
+  const router = useRouter();
   const [user, setUser] = useState<null | any>(null);
+
+  useEffect(() => {
+   async function GetInfoProfile(){
+    const token =  Cookies.get('jwt');
+    const data = await instance.post('/api/user/profile', {
+      token: token
+    })
+    if(data) {
+      setUser(data?.data)
+     }
+   }
+  
+   GetInfoProfile();
+  }, [setUser])
+
+   function RouterMoveUser(){
+    if(user?.email){
+      if (pathName === "/login" || pathName == "/register") {
+        router.push("/");
+      }
+    }
+  }
+  RouterMoveUser();
 
   async function RegisterUser({name, password, email}:any) {
     const data = await instance.post("/api/user", {
@@ -18,11 +43,13 @@ export default function AuthContextProvider({ children }: IChildren) {
     })
     .then((data) => {
       console.log(data?.data);
+      Cookies.set('jwt', data?.data?.token, {expires: 7})
+      location.reload();
     })
     .catch((error) => {
       toast({
         title: "Não foi possível criar esse usuário.",
-        description: error?.response?.data,
+        description: error?.response?.data || Date.now(),
       })
     })
   }
